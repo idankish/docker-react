a2e2e1660e2266ac91da96c5d3c00d016c645d93
idankish <IdanKish78@gmail.com>
2024-02-20 18:55:42 +0200

    changing text in App.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Blat Suka Nachuy!
+          Learn React - Hello from Frontend app!
         </a>
       </header>
     </div>
